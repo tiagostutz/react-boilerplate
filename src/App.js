0145556ef7067c40bc84f58dbb37c7e6e6c88a90
stutzsolucoes <tiago@stutzsolucoes.com.br>
2016-01-28 00:20:59 -0200

    improved webpack with dev server. Starting Rhelean integration.
    Improved webpack support so it now has the hot reload and the webpack
    static file serve to make it easy to debug the components instead of
    the flattened JS files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
-    return <h1>This is a react starter !</h1>
+    return <h1>This is a React Rheleana starter !</h1>
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 var path = require('path');
-var node_modules = path.resolve(__dirname, 'node_modules');
-var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
-var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
+var webpack = require('webpack');
 
 module.exports = {
-  entry : './main.js',
-  resolve: {
-      alias: {
-        'react': pathToReact, //usar a versao min
-        'react-dom': pathToReactDOM //usar a versao min
-      }
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
-  output : {
-    path : './',
-    filename : 'index.js'
-  },
-  devServer : {
-    inline: true,
-    port: 1111
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.js$/,
-        exclude : /node_modules/,
-        loader : 'babel'
-      }
-    ],
-    noParse: [pathToReact] //evitar que 'compile' toda vez esses scripts que ja sao consolidados
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"',
+      },
+      __DEVELOPMENT__: true,
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }]
   }
-}
+};
